7cd20601e6f1ea2cbeb04c5bd679fa061e835d53
Qori Chairawan <qorichairawan17@gmail.com>
2024-12-18 18:12:15 +0700

    #fix : remove setting layout

--- resources/views/layout/body.blade.php
+++ resources/views/layout/body.blade.php
@@ -343,32 +343,6 @@
                 <li class="dropdown pc-h-item">
                     <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
                         role="button" aria-haspopup="false" aria-expanded="false">
-                        <i class="ph-duotone ph-sun-dim"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-end pc-h-dropdown">
-                        <a href="#!" class="dropdown-item" onclick="layout_change('dark')">
-                            <i class="ph-duotone ph-moon"></i>
-                            <span>Dark</span>
-                        </a>
-                        <a href="#!" class="dropdown-item" onclick="layout_change('light')">
-                            <i class="ph-duotone ph-sun-dim"></i>
-                            <span>Light</span>
-                        </a>
-                        <a href="#!" class="dropdown-item" onclick="layout_change_default()">
-                            <i class="ph-duotone ph-cpu"></i>
-                            <span>Default</span>
-                        </a>
-                    </div>
-                </li>
-                <li class="pc-h-item">
-                    <a class="pc-head-link pct-c-btn" href="#" data-bs-toggle="offcanvas"
-                        data-bs-target="#offcanvas_pc_layout">
-                        <i class="ph-duotone ph-gear-six"></i>
-                    </a>
-                </li>
-                <li class="dropdown pc-h-item">
-                    <a class="pc-head-link dropdown-toggle arrow-none me-0" data-bs-toggle="dropdown" href="#"
-                        role="button" aria-haspopup="false" aria-expanded="false">
                         <i class="ph-duotone ph-bell"></i>
                         <span class="badge bg-success pc-h-badge">3</span>
                     </a>
