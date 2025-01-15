a251d4bf9266816ddffd938209e1852fffae5b69
qoryy17 <qorichairawan17@gmail.com>
2025-01-15 10:33:40 +0700

    #fix : counter number

--- app/Http/Controllers/Manajemen/RapatController.php
+++ app/Http/Controllers/Manajemen/RapatController.php
@@ -105,7 +105,8 @@ class RapatController extends Controller
             }
 
             // Generate index nomor dokumen rapat
-            $indexNumber = ManajemenRapatModel::orderBy('nomor_indeks', 'desc')->lockForUpdate()->first();
+            // $indexNumber = ManajemenRapatModel::orderBy('nomor_indeks', 'desc')->lockForUpdate()->first();
+            $indexNumber = ManajemenRapatModel::orderBy('nomor_indeks', 'desc')->first();
             if (!$indexNumber) {
                 $counter = 0;
             } else {
